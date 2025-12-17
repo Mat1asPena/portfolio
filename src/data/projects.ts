95dd1c28fd1160c7db3c21ba69cb77882c2ed605
Matias Agustin Peña <148594557+Mat1asPena@users.noreply.github.com>
2025-12-17 14:22:15 -0300

    first commit

---
+++ src/data/projects.ts
@@ -0,0 +1,12 @@
+export const projects = [
+    { 
+        // ejemplo de proyecto : 
+        title: "ElectroGaming",
+        slug: "electrogaming",
+        tech: ["Angular", "JavaScript", "CSS"],
+        description: "Ecommerce con carrito y localStorage",
+        github: "https://github.com/...",
+        demo: "https://...",
+        highlight: true
+    }
+];
